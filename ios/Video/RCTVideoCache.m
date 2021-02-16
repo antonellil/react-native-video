a8836ce138dce08f6b9fae1feb0dda5233e6f435
Louis Antonelli <lpa22@cornell.edu>
2021-02-16 17:45:35 -0600

    revert temp storage

--- ios/Video/RCTVideoCache.m
+++ ios/Video/RCTVideoCache.m
@@ -5,6 +5,7 @@
 @synthesize videoCache;
 @synthesize cachePath;
 @synthesize cacheIdentifier;
+@synthesize temporaryCachePath;
 
 + (RCTVideoCache *)sharedInstance {
   static RCTVideoCache *sharedInstance = nil;
@@ -18,25 +19,38 @@
 - (id)init {
   if (self = [super init]) {
     self.cacheIdentifier = @"rct.video.cache";
+    self.temporaryCachePath = [NSTemporaryDirectory() stringByAppendingPathComponent:self.cacheIdentifier];
     self.cachePath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject stringByAppendingPathComponent:self.cacheIdentifier];
     SPTPersistentCacheOptions *options = [SPTPersistentCacheOptions new];
     options.cachePath = self.cachePath;
     options.cacheIdentifier = self.cacheIdentifier;
     options.defaultExpirationPeriod = 60 * 60 * 24 * 30;
     options.garbageCollectionInterval = (NSUInteger)(1.5 * SPTPersistentCacheDefaultGCIntervalSec);
-    options.sizeConstraintBytes = 1024 * 1024 * 100; // 100 MB
+    options.sizeConstraintBytes = 1024 * 1024 * 100;
     options.useDirectorySeparation = NO;
-
 #ifdef DEBUG
     options.debugOutput = ^(NSString *string) {
       NSLog(@"Dat Video Cache: %@", string);
     };
 #endif
-
+    [self createTemporaryPath];
     self.videoCache = [[SPTPersistentCache alloc] initWithOptions:options];
     [self.videoCache scheduleGarbageCollector];
   }
   return self;
+}
+
+- (void) createTemporaryPath {
+  NSError *error = nil;
+  BOOL success = [[NSFileManager defaultManager] createDirectoryAtPath:self.temporaryCachePath
+                                           withIntermediateDirectories:YES
+                                                            attributes:nil
+                                                                 error:&error];
+#ifdef DEBUG
+  if (!success || error) {
+    NSLog(@"Error while! %@", error);
+  }
+#endif
 }
 
 - (void)storeItem:(NSData *)data forUri:(NSString *)uri withCallback:(void(^)(BOOL))handler;
@@ -46,6 +60,7 @@
     handler(NO);
     return;
   }
+  [self saveDataToTemporaryStorage:data key:key];
   [self.videoCache storeData:data forKey:key locked:NO withCallback:^(SPTPersistentCacheResponse * _Nonnull response) {
     if (response.error) {
 #ifdef DEBUG
@@ -57,6 +72,24 @@
     handler(YES);
   } onQueue:dispatch_get_main_queue()];
   return;
+}
+
+- (AVURLAsset *)getItemFromTemporaryStorage:(NSString *)key {
+  NSString * temporaryFilePath = [self.temporaryCachePath stringByAppendingPathComponent:key];
+
+  BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:temporaryFilePath];
+  if (!fileExists) {
+    return nil;
+  }
+  NSURL *assetUrl = [[NSURL alloc] initFileURLWithPath:temporaryFilePath];
+  AVURLAsset *asset = [AVURLAsset URLAssetWithURL:assetUrl options:nil];
+  return asset;
+}
+
+- (BOOL)saveDataToTemporaryStorage:(NSData *)data key:(NSString *)key {
+  NSString *temporaryFilePath = [self.temporaryCachePath stringByAppendingPathComponent:key];
+  [data writeToFile:temporaryFilePath atomically:YES];
+  return YES;
 }
 
 - (NSString *)generateCacheKeyForUri:(NSString *)uri {
@@ -96,15 +129,19 @@
 - (void)getItemForUri:(NSString *)uri withCallback:(void(^)(RCTVideoCacheStatus, AVAsset * _Nullable)) handler {
   @try {
     NSString *key = [self generateCacheKeyForUri:uri];
+    AVURLAsset * temporaryAsset = [self getItemFromTemporaryStorage:key];
+    if (temporaryAsset != nil) {
+      handler(RCTVideoCacheStatusAvailable, temporaryAsset);
+      return;
+    }
 
     [self.videoCache loadDataForKey:key withCallback:^(SPTPersistentCacheResponse * _Nonnull response) {
       if (response.record == nil || response.record.data == nil) {
         handler(RCTVideoCacheStatusNotAvailable, nil);
         return;
       }
-
-      handler(RCTVideoCacheStatusNotAvailable, nil);
-      // handler(RCTVideoCacheStatusAvailable, response.record.data);
+      [self saveDataToTemporaryStorage:response.record.data key:key];
+      handler(RCTVideoCacheStatusAvailable, [self getItemFromTemporaryStorage:key]);
     } onQueue:dispatch_get_main_queue()];
   } @catch (NSError * err) {
     switch (err.code) {
